8cbccf18de509b87b5a09e50199f4ee5cba93db4
Meet Agrawal <maagrawal2112@gmail.com>
2026-02-07 19:25:08 +0530

    frontend setup and basic landing page design

---
+++ frontend /my-react-app/src/pages/landing.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import '../App.css'
+import { Link } from 'react-router-dom'
+function LandingPage() {
+  return (
+    <div className='LandingPageContainer'>
+      <nav> 
+        <div className='navLeft'>
+          <h2>V Call</h2>
+        </div>
+        <div className='navRight'>
+          <p>Join As Guest</p>
+          <p>Register</p>
+          <div role='button'>
+            Sign In
+          </div>
+        </div>
+      </nav>
+
+      <div className="Hero">
+        <div className='heroText'>
+          <h1><span style={{ color: "#ff9999" }}>Connect</span> With The Loved Ones</h1>
+          <p>Connect Far in Seconds</p>
+
+          <div role='button' className='getStarted'>
+            <Link to="/home">Get Started</Link>
+          </div>
+
+        </div>
+
+        <div className='mobileSecondary'>
+          <img src="/img2.jpg" alt="Img" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
